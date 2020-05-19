5cecf92d8793f305fced72c6968d596151a4b86a
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-05-19 20:34:54 +0530

    Displaying saved images in ListView

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
+import 'package:favoritelocations/providers/great_places.dart';
 import 'package:favoritelocations/screens/add_place_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
@@ -16,8 +18,24 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        child: Center(
+          child: const Text('Got no places yet, start adding some!'),
+        ),
+        builder: (context, greatPlaces, child) => greatPlaces.items.length <= 0
+            ? child
+            : ListView.builder(
+                itemCount: greatPlaces.items.length,
+                itemBuilder: (context, index) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(
+                      greatPlaces.items[index].image,
+                    ),
+                  ),
+                  title: Text(greatPlaces.items[index].title),
+                  onTap: () {},
+                ),
+              ),
       ),
     );
   }
